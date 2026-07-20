fd83939d8b33bf9fc895e3e0744cd2e6ff7d8df0
lieselypark-hash <lieselypark@gmail.com>
2026-07-20 09:34:13 -0700

    Add configuration class for training parameters

---
+++ config.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+
+    ########################
+    # Environment
+    ########################
+
+    env_name = "PandaPickAndPlace-v3"
+
+    ########################
+    # Training
+    ########################
+
+    episodes = 500
+    max_steps = 250
+
+    gamma = 0.99
+
+    actor_lr = 3e-4
+    critic_lr = 3e-4
+
+    batch_size = 256
+
+    replay_size = 100000
+
+    tau = 0.005
+
+    ########################
+    # Networks
+    ########################
+
+    hidden_dim = 256
+
+    ########################
+    # Exploration
+    ########################
+
+    exploration_noise = 0.1
+
+    ########################
+    # Saving
+    ########################
+
+    checkpoint_path = "checkpoints"
+
+    ########################
+    # Randomness
+    ########################
+
+    seed = 42
