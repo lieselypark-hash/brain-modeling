88a21626abb55d41dacfabed766880cf404d649a
lieselypark-hash <lieselypark@gmail.com>
2026-07-22 10:02:40 -0700

    Define public API in brain module
    Add imports and define __all__ for public API.

--- brain/__init__.py
+++ brain/__init__.py
@@ -1,1 +1,11 @@
+from .actor import Actor
+from .critic import Critic
+from .dopamine_system import DopamineSystem
+from .trainer import Trainer
 
+__all__ = [
+    "Actor",
+    "Critic",
+    "DopamineSystem",
+    "Trainer",
+]
